02b0e7be79a69eceb44183333d928ab9c07a90c1
tanmaycreationtool-beep <tanmaycreationtool@gmail.com>
2026-09-06 11:55:06 +0530

    Prepare Android shell for UI Studio

--- app/src/main/java/com/deadline/android/MainActivity.kt
+++ app/src/main/java/com/deadline/android/MainActivity.kt
@@ -4,16 +4,28 @@ import android.app.Activity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.view.Window
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val web = WebView(this)
-        web.settings.javaScriptEnabled = true
-        web.settings.domStorageEnabled = true
-        web.settings.allowFileAccess = true
-        web.webViewClient = WebViewClient()
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setStatusBarColor(android.graphics.Color.rgb(5, 5, 5))
+        window.setNavigationBarColor(android.graphics.Color.rgb(5, 5, 5))
+
+        val web = WebView(this).apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.allowFileAccess = true
+            settings.allowContentAccess = true
+            webViewClient = WebViewClient()
+        }
         setContentView(web)
         web.loadUrl("file:///android_asset/index.html")
+    }
+
+    override fun onBackPressed() {
+        val web = findViewById<WebView>(android.R.id.content)?.getChildAt(0) as? WebView
+        if (web?.canGoBack() == true) web.goBack() else super.onBackPressed()
     }
 }
