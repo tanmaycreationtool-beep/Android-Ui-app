0a0e566f32824933de184ef86e49c434538f156d
tanmaycreationtool-beep <tanmaycreationtool@gmail.com>
2026-09-06 11:49:15 +0530

    Add Deadline Android activity

---
+++ app/src/main/java/com/deadline/android/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.deadline.android
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val web = WebView(this)
+        web.settings.javaScriptEnabled = true
+        web.settings.domStorageEnabled = true
+        web.settings.allowFileAccess = true
+        web.webViewClient = WebViewClient()
+        setContentView(web)
+        web.loadUrl("file:///android_asset/index.html")
+    }
+}
