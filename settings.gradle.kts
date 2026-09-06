161c9cb5ea6a646d2b6fb4a5b6854882c59c93c1
tanmaycreationtool-beep <tanmaycreationtool@gmail.com>
2026-09-06 12:09:52 +0530

    Remove remaining old app naming from Android project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,6 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
-rootProject.name = "DeadlineAndroid"
+rootProject.name = "UIStudioAndroid"
 include(":app")
