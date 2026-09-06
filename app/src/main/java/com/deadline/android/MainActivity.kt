002ad7ad7b1ebd59b1352fec032aadd6a17cc68d
tanmaycreationtool-beep <tanmaycreationtool@gmail.com>
2026-09-06 12:10:01 +0530

    Remove legacy package naming from app code

--- app/src/main/java/com/deadline/android/MainActivity.kt
+++ app/src/main/java/com/deadline/android/MainActivity.kt
@@ -1,17 +1,18 @@
-package com.deadline.android
+package com.uistudio.android
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Window
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.view.Window
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setStatusBarColor(android.graphics.Color.rgb(5, 5, 5))
-        window.setNavigationBarColor(android.graphics.Color.rgb(5, 5, 5))
+        window.statusBarColor = Color.rgb(5, 5, 5)
+        window.navigationBarColor = Color.rgb(5, 5, 5)
 
         val web = WebView(this).apply {
             settings.javaScriptEnabled = true
@@ -22,10 +23,5 @@ class MainActivity : Activity() {
         }
         setContentView(web)
         web.loadUrl("file:///android_asset/index.html")
-    }
-
-    override fun onBackPressed() {
-        val web = findViewById<WebView>(android.R.id.content)?.getChildAt(0) as? WebView
-        if (web?.canGoBack() == true) web.goBack() else super.onBackPressed()
     }
 }
